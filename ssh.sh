a110332d504cb7309c8bb2b464e1c36096257ae4
Sean Moloney <s_moloney@aol.com>
2019-06-06 19:21:02 -0700

    Finished

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,27 @@
 #!/bin/bash -p
 
+remove(){
+	echo $1
+	if [ -f "$log/$1" ]; then
+		rm ${log}/${1}
+	fi
+}
+
+spiro_and_arete(){
+	echo "Raid Health"
+	ssh root@$1 "/usr/local/bin/raid_health.sh && cat /tmp/smart_report.tmp" | awk '/SMART/{print $0}' >> ${log}/$1.txt
+
+	printf "\n\n" >> ${log}/$1.txt
+	echo "Top Processes"
+	ssh root@$1 top >> ${log}/$1.txt
+}
+lys_buc_arist(){
+	echo "Raid Health"
+	ssh root@$1 "/usr/local/bin/cciss_vol_status /dev/cciss/c0d0 -V" >> ${log}/$1.txt
+	printf "\n\n" >> ${log}/$1.txt
+	echo "Top Processes"
+	ssh root@$1 "top -n 1 -b|grep \"load average\" -A15" >> ${log}/$1.txt
+}
 PHASE="ALL"
 	while getopts ":a:" opt; do
 		case $opt in
@@ -12,55 +34,107 @@ shift $((OPTIND -1))
 
 echo $PHASE
 log=$(pwd)/log
+if [ ! -d "${log}" ]; then
+	mkdir -p $log
+fi
 
 if [ "${PHASE}" = "ALL" ] || [ "${PHASE}" = "spiro" ]; then
-	if [ -f ${log}/spiro.txt ]; then
-		rm ${log}/spiro.txt
-	fi
+
+	remove spiro.txt
+
 	printf " ----------\n Spiro\n ----------\n"
 	printf "################\n" >> ${log}/spiro.txt
 	printf "#	Spiro	     #\n" >> ${log}/spiro.txt
 	printf "################\n" >> ${log}/spiro.txt
 	printf "$(date)\n\n" >> ${log}/spiro.txt
-	echo "Raid Health"
-	ssh root@spiro "/usr/local/bin/raid_health.sh && cat /tmp/smart_report.tmp" | awk '/SMART/{print $0}' >> ${log}/spiro.txt
+	spiro_and_arete spiro
 
-	printf "\n\n" >> ${log}/spiro.txt
-	echo "Top Processes"
-	ssh root@spiro top >> ${log}/spiro.txt
 fi
 
 if [ "${PHASE}" = "ALL" ] || [ "$PHASE" = "arete" ]; then
-	if [ -f ${log}/arete.txt ]; then
-		rm ${log}/arete.txt
-	fi
+
+	remove arete.txt
+
 	printf " ----------\n Arete\n ----------\n"
 	printf "################\n" >> ${log}/arete.txt
 	printf "#	Arete	     #\n" >> ${log}/arete.txt
 	printf "################\n" >> ${log}/arete.txt
 	printf "$(date)\n\n" >> ${log}/arete.txt
-	echo "Raid Health"
-	ssh root@arete "/usr/local/bin/raid_health.sh && cat /tmp/smart_report.tmp" | awk '/SMART/{print $0}' >> ${log}/arete.txt
-	printf "\n\n" >> ${log}/arete.txt
-	echo "Top Processes" 
-	ssh root@arete top >> ${log}/arete.txt
-	# cat ${log}/arete.txt
+
+	spiro_and_arete arete
+
 fi
 if [ "${PHASE}" = "ALL" ] || [ "${PHASE}" = "lystrata" ]; then
-	if [ "$(ls -A $log/lystrata.txt)" ]; then
-		rm ${log}/lystrata.txt
-	fi
+		remove lystrata.txt
 	printf "################\n" >> ${log}/lystrata.txt
 	printf "#	Lystrata	#\n" >> ${log}/lystrata.txt
 	printf "################\n" >> ${log}/lystrata.txt
 
 	printf " ----------\n Lystrata\n ----------\n"
 	printf "$(date)\n\n" >> ${log}/lystrata.txt
-	echo "Raid Health"
-	ssh root@lystrata "/usr/local/bin/cciss_vol_status /dev/cciss/c0d0 -V" >> ${log}/lystrata.txt
-	printf "\n\n" >> ${log}/lystrata.txt
-	echo "Top Processes"
-	ssh root@lystrata "top -n 1 -b|grep \"load average\" -A15" >> ${log}/lystrata.txt
+
+	lys_buc_arist lystrata
 
 fi
-echo "All done :).  Logs are located in ${log}/log"
+
+if [ "${PHASE}" = "ALL" ] || [ "${PHASE}" = "bucephalus" ]; then
+		remove bucephalus.txt
+	printf "################\n" >> ${log}/bucephalus.txt
+	printf "#	Bucephalus	#\n" >> ${log}/bucephalus.txt
+	printf "################\n" >> ${log}/bucephalus.txt
+
+	printf " ----------\n Bucephalus\n ----------\n"
+	printf "$(date)\n\n" >> ${log}/bucephalus.txt
+		lys_buc_arist bucephalus
+
+fi
+
+if [ "${PHASE}" = "ALL" ] || [ "${PHASE}" = "aristippus" ]; then
+		remove aristippus.txt
+	printf "################\n" >> ${log}/aristippus.txt
+	printf "#	Aristippus	#\n" >> ${log}/aristippus.txt
+	printf "################\n" >> ${log}/aristippus.txt
+
+	printf " ----------\n Aristippus\n ----------\n"
+	printf "$(date)\n\n" >> ${log}/aristippus.txt
+		lys_buc_arist aristippus
+
+fi
+
+if [ "${PHASE}" = "ALL" ] || [ "${PHASE}" = "batty" ]; then
+	remove batty.txt
+
+	printf " ----------\n Batty\n ----------\n"
+	printf "################\n" >> ${log}/batty.txt
+	printf "#	Batty	     #\n" >> ${log}/batty.txt
+	printf "################\n" >> ${log}/batty.txt
+	printf "$(date)\n\n" >> ${log}/batty.txt
+
+	echo "Raid health"
+	ssh root@batty "tw_cli /c2 show" >> ${log}/batty.txt
+	echo "Top processes"
+	ssh root@batty "top -n 1 -b|grep \"load average\" -A15" >> ${log}/batty.txt
+
+
+fi
+
+if [ "${PHASE}" = "ALL" ] || [ "${PHASE}" = "morrison" ]; then
+	remove morrison.txt
+
+	printf " ----------\n Morrison\n ----------\n"
+	printf "################\n" >> ${log}/morrison.txt
+	printf "#	Morrison	     #\n" >> ${log}/morrison.txt
+	printf "################\n" >> ${log}/morrison.txt
+	printf "$(date)\n\n" >> ${log}/morrison.txt
+
+	echo "Raid health"
+	ssh root@morrison /opt/hp/hpssacli/bld/hpssacli ctrl all show config >> ${log}/morrison.txt
+
+	echo "Top processes"
+	ssh root@morrison "top -n 1 -b|grep \"load average\" -A15" >> ${log}/morrison.txt
+
+
+fi
+
+
+echo "All done :).  Logs are located in ${log}"
